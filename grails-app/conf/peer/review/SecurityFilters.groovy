bd10950c2c0bfebd6a9ec9cde429d61515d2bf6f
ursoos <ursoos@gmail.com>
2013-09-01 13:13:24 +0200

    Added Security Filters

---
+++ grails-app/conf/peer/review/SecurityFilters.groovy
@@ -0,0 +1,19 @@
+package peer.review
+
+class SecurityFilters {
+
+
+	def filters = {
+		loginCheck(controller: '*', action: '*') {
+			before = {
+//				if (!session.user && actionName != "login") {
+//					redirect(controller: "person", action: "login")
+//
+//					return true
+//				}
+			}
+		}
+	}
+}
+
+
